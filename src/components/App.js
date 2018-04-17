eb3a0e36ae75028fe92151c131daa44cf7736c13
samsjchi <samuel.sj.chi@gmail.com>
2018-04-17 00:24:32 -0400

    Create project tiles structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Landing from './Landing';
+import ProjectTiles from './ProjectTiles';
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div class='content-container'>
         <Landing />
+        <ProjectTiles />
       </div>
     );
   }
