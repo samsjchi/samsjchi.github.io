29942b05ed4944e7b9d4c2202b5ebe9e38a3f248
samsjchi <samuel.sj.chi@gmail.com>
2018-04-18 12:51:42 -0400

    Create VMware component

--- src/components/ProjectTiles/Projects/VMware.js
+++ src/components/ProjectTiles/Projects/VMware.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import vmwareImg from '../../../../assets/img/vmware-the-possibility-report.jpg';
+import vmwareImg from '../../../../assets/img/vmware-possibility-report.jpg';
 
 export default function VMware () {
   return (
-    <li className='project-tiles__tile'>
+    <li id='vmware' className='project-tiles__tile'>
       <div className='project-tiles__tile-content'>
         <span className='project-tiles__client'>VMware x The Atlantic</span>
         <h2 className='project-tiles__title'>The Possibility Report</h2>
         <p className='project-tiles__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
+      
       <div className='project-tiles__cta'>View Project →</div>
-      <div className='project-tiles__image' data-bg-start='#C6D1E3' data-bg-end='#1C505D'>
+      
+      <div className='project-tiles__image' data-bg-start='#061454' data-bg-end='#fc4a1a'>
         <img 
           className='b-lazy' 
           src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
