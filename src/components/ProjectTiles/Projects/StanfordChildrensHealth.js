ede183b9da2553bc9215bcb2f6f4290c6c5a2c46
samsjchi <samuel.sj.chi@gmail.com>
2018-04-18 12:55:53 -0400

    Create StanfordChildrensHealth component

---
+++ src/components/ProjectTiles/Projects/StanfordChildrensHealth.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import stanfordChildrensHealthImg from '../../../../assets/img/stanford-childrens-health.jpg';
+
+export default function StanfordChildrensHealth () {
+  return (
+    <li id='stanford' className='project-tiles__tile'>
+      <div className='project-tiles__tile-content'>
+        <span className='project-tiles__client'>Stanford Children's Health x The Atlantic</span>
+        <h2 className='project-tiles__title'>Building with Care</h2>
+        <p className='project-tiles__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      
+      <div className='project-tiles__cta'>View Project →</div>
+      
+      <div className='project-tiles__image' data-bg-start='#2D2114' data-bg-end='#c19b72'>
+        <img 
+          className='b-lazy' 
+          src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+          data-src={stanfordChildrensHealthImg}
+         />
+      </div>
+    </li>
+  );
+}
