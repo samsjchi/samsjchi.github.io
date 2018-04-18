34a7b7c62dec63e181c4ea6100dd1a51ea130c3a
samsjchi <samuel.sj.chi@gmail.com>
2018-04-18 18:22:26 -0400

    Add reveal animation to intro copy

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,8 +4,10 @@ export default function Landing () {
   return (
     <div className='landing'>
       <div className='intro'>
-        <h2 className='intro__line-1'>I'm Sam Chi – a front end engineer based in New York City. Currently at <a href="https://advertising.theatlantic.com/rethink" target="_blank">Atlantic Re:think</a>, The Atlantic's in-house creative marketing team and award-winning content studio.</h2>
-        <h2 className='intro__line-2'>I design & build dynamic web applications.</h2>
+        <h2>I'm Sam Chi – a front end engineer based in New York City.</h2>
+        <h2>Currently at <a href="https://advertising.theatlantic.com/rethink" target="_blank">Atlantic Re:think</a>, The Atlantic's in-house</h2>
+        <h2>creative marketing team and award-winning content studio.</h2>
+        <h2>I design & build dynamic web applications.</h2>
       </div>
     </div>
   )
