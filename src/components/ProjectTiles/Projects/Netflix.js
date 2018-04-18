6cffc955f94954ad67eb16f3ba5ab3ebec18c44a
samsjchi <samuel.sj.chi@gmail.com>
2018-04-18 12:56:50 -0400

    Update gradient colors

--- src/components/ProjectTiles/Projects/Netflix.js
+++ src/components/ProjectTiles/Projects/Netflix.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-import NetflixFaudaImg from '../../../../assets/img/netflix-fauda.jpg';
+import NetflixImg from '../../../../assets/img/netflix-fauda.jpg';
 
 export default function Netflix () {
   return (
-    <li className='project-tiles__tile'>
+    <li id='netflix' className='project-tiles__tile'>
       <div className='project-tiles__tile-content'>
         <span className='project-tiles__client'>Netflix x The Atlantic</span>
         <h2 className='project-tiles__title'>Fauda</h2>
         <p className='project-tiles__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
+      
       <div className='project-tiles__cta'>View Project →</div>
-      <div className='project-tiles__image' data-bg-start='#C6D1E3' data-bg-end='#1C505D'>
+
+      <div className='project-tiles__image' data-bg-start='#1A3342' data-bg-end='#030303'>
         <img 
           className='b-lazy' 
           src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
-          data-src={NetflixFaudaImg}
+          data-src={NetflixImg}
          />
       </div>
     </li>
