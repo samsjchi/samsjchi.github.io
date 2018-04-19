52d3a94d604ca00100a41035924166dca6d57f70
samsjchi <samuel.sj.chi@gmail.com>
2018-04-18 20:23:41 -0400

    Add last name pronunciation tooltip

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,16 @@ export default function Landing () {
   return (
     <div className='landing'>
       <div className='intro'>
-        <h2>I'm Sam Chi – a front end engineer based in New York City.</h2>
+        <h2>I'm Sam 
+          <div className='tooltip'>
+            <span className='tooltip__message'>
+              Pronounced “kai,” like the Greek letter
+            </span>
+            Chi
+          </div>
+          
+          – a front end developer based in New York City.
+        </h2>
         <h2>Currently at <a className='rethink' href="https://advertising.theatlantic.com/rethink/" target="_blank">Atlantic Re:think</a>, the award-winning content</h2>
         <h2>studio and in-house creative marketing team at <a className='atlantic' href="https://www.theatlantic.com/" target="_blank">The Atlantic</a>.</h2>
         <h2>I design & build dynamic web applications.</h2>
