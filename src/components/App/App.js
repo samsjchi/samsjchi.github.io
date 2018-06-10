f3a30637b582da04ee6a1a971e6c509dd2ea2204
Sam Chi <samuel.sj.chi@gmail.com>
2018-06-10 17:08:36 -0400

    Fix ESLint errors in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SideNavLeft from '../SideNavLeft/SideNavLeft';
 import Landing from '../Landing/Landing';
 import ProjectTiles from '../ProjectTiles/ProjectTiles';
 import Footer from '../Footer/Footer';
 
-class App extends Component {
-  render () {
-    return (
-      <div className='app'>
-        <div className='content-container'>
-          <SideNavLeft />
-          <Landing />
-          <ProjectTiles />
-        </div>
-        <Footer />
+export default function App() {
+  return (
+    <div className="app">
+      <div className="content-container">
+        <SideNavLeft />
+        <Landing />
+        <ProjectTiles />
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
 }
-
-export default App;
