314156892d0fcb74fd912c4b5dd0ab48330c748d
samsjchi <samuel.sj.chi@gmail.com>
2018-07-10 15:03:29 -0400

    Fix ESLint errors

--- webpack.config.js
+++ webpack.config.js
@@ -9,49 +9,58 @@ module.exports = {
   },
   module: {
     rules: [
-      { 
-        test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ 
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
-      { 
+      {
         test: /\.scss$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'sass-loader'
-        }]
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
       },
-      { 
-        test: /\.(jpe?g|png|gif)$/i, 
-        use: 'url-loader?limit=25000'
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: 'url-loader?limit=25000',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader'
+        use: 'file-loader',
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: 'babel-loader'
+            loader: 'babel-loader',
           },
           {
             loader: 'react-svg-loader',
             options: {
-              jsx: true
-            }
-          }
-        ]
+              jsx: true,
+            },
+          },
+        ],
       },
     ],
   },
 
-  plugins: process.argv.indexOf('-p') === -1 ? [] : [
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false,
-      },
-    }),
-  ],
+  plugins:
+    process.argv.indexOf('-p') === -1
+      ? []
+      : [
+        new webpack.optimize.UglifyJsPlugin({
+          output: {
+            comments: false,
+          },
+        }),
+      ],
 };
