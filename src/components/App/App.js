a2054e76b86359562a094b6d44e16ddd9fc4c48d
samsjchi <samuel.sj.chi@gmail.com>
2018-07-10 18:27:45 -0400

    Add Nav component and link to Photography, About, Contact pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
+import Nav from '../Nav/Nav';
 import SideNavLeft from '../SideNavLeft/SideNavLeft';
 import Landing from '../Landing/Landing';
-import ProjectTiles from '../ProjectTiles/ProjectTiles';
 import Footer from '../Footer/Footer';
+
+const About = () => (
+  <div className="temp-wrapper">
+    <h1>About</h1>
+  </div>
+);
+
+const Photography = () => (
+  <div className="temp-wrapper">
+    <h1>Photography</h1>
+  </div>
+);
+
+const Contact = () => (
+  <div className="temp-wrapper">
+    <h1>Contact</h1>
+  </div>
+);
+
+const PageNotFound = () => (
+  <div className="temp-wrapper">
+    <h1>404: Page Not Found</h1>
+  </div>
+);
 
 export default function App() {
   return (
     <div className="app">
-      <div className="content-container">
+      <Nav />
+
+      <div className="main-content">
         <SideNavLeft />
-        <Landing />
-        <ProjectTiles />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/photography" component={Photography} />
+          <Route path="/contact" component={Contact} />
+          <Route component={PageNotFound} />
+        </Switch>
       </div>
+
       <Footer />
     </div>
   );
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <div className="nav-wrapper">
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/photography">Photography</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
